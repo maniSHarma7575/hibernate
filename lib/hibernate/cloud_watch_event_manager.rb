82d0271421d48a781a702e9e717fb7ed23a8b0dd
maniSHarma7575 <hacker.sharma7575@gmail.com>
2024-09-30 11:59:56 +0530

    Lambda triggers registration with cloudwatch

--- lib/hibernate/cloud_watch_event_manager.rb
+++ lib/hibernate/cloud_watch_event_manager.rb
@@ -1,5 +1,6 @@
 require 'aws-sdk-cloudwatchevents'
 require 'json'
+require 'dotenv/load'
 
 class CloudWatchEventManager
   def initialize(events_client, instance_id, instance_name, lambda_function_arn)
@@ -7,6 +8,9 @@ class CloudWatchEventManager
     @instance_id = instance_id
     @instance_name = instance_name
     @lambda_function_arn = lambda_function_arn
+    @aws_region = ENV['AWS_REGION']
+    @account_id = ENV['ACCOUNT_ID']
+    @lambda_client = Aws::Lambda::Client.new(region: @aws_region)
   end
 
   def create_start_rule(cron_expression)
@@ -37,6 +41,8 @@ class CloudWatchEventManager
       description: "Rule to #{action} EC2 instance #{@instance_id} (Name: #{@instance_name}) at specified time: cron(#{cron_expression})",
     })
 
+    add_lambda_permission(rule_name)
+
     @events_client.put_targets({
       rule: rule_name,
       targets: [
@@ -49,5 +55,21 @@ class CloudWatchEventManager
     })
 
     puts "#{action.capitalize} rule created for instance '#{@instance_name}' (ID: #{@instance_id})."
+  end
+
+  def add_lambda_permission(rule_name)
+    begin
+      @lambda_client.add_permission({
+        function_name: @lambda_function_arn,
+        statement_id: "#{rule_name}-Permission",
+        action: "lambda:InvokeFunction",
+        principal: "events.amazonaws.com",
+        source_arn: "arn:aws:events:#{@aws_region}:#{@account_id}:rule/#{rule_name}"
+      })
+
+      puts "Permission added for rule #{rule_name} to invoke Lambda #{@lambda_function_arn}."
+    rescue Aws::Lambda::Errors::ResourceConflictException => e
+      puts "Permission already exists: #{e.message}"
+    end
   end
 end
